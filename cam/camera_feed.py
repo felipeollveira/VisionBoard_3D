55b32428761c924cd8b3bac0b2003e43a1b1fb90
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2025-02-01 17:37:28 -0300

    :feature camera get all screen

--- cam/camera_feed.py
+++ cam/camera_feed.py
@@ -4,12 +4,15 @@ from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt, QTimer
 from .update_frames import update_frame
 
+
 class CameraFeed(QWidget):
     def __init__(self):
         super().__init__()
+
+        # Configurar para mostrar a janela em tela cheia
         self.setWindowTitle("Camera Feed")
-        self.setGeometry(50, 50, 640, 480)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.showFullScreen()  # Torna a janela em tela cheia
+        self.setWindowFlags(Qt.FramelessWindowHint)  # Remove as bordas da janela
         self.setStyleSheet("background-color: gray;")
 
         self.label = QLabel("Inicializando câmera...", self)
@@ -24,7 +27,7 @@ class CameraFeed(QWidget):
             self.timer = QTimer(self)
             self.timer.timeout.connect(self.update_frame)  # Chama a função de atualização do frame
             self.timer.start(30)
-
+    
     # Método para chamar a função update_frame do arquivo update_frames.py
     def update_frame(self):
         update_frame(self)
